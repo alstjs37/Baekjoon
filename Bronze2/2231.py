bc34c948bd94f2d9e0ea0925289bb5fbef8901b9
alstjs37 <97160888+alstjs37@users.noreply.github.com>
2022-01-24 16:54:01 +0900

    Add files via upload

---
+++ Bronze2/2231.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+result = 0
+for i in range(1, n+1) :
+    arr = list(map(int, str(i)))
+    result = i + sum(arr) 
+    if result == n :
+        print(i)
+        break
+else :
+    print(0)
